9ac2e86ffe8c50f51adc5604420e8583d44d8d8f
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-30 11:17:35 +0200

    turbo

--- src/app/@modal/(.)contact/page.tsx
+++ src/app/@modal/(.)contact/page.tsx
@@ -2,5 +2,9 @@ import { Contact } from '@/app/components/Contact/Contact';
 import { Modal } from './modal';
 
 export default function PageModal() {
-  return <Modal>.</Modal>;
+  return (
+    <Modal>
+      <Contact />
+    </Modal>
+  );
 }
