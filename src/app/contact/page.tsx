2247befa562eeb340a8809cb0a50333149460dd0
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-30 11:01:34 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 import { Contact } from '../components/Contact/Contact';
 
-export default function Contacts({ children }: { children: React.ReactNode }) {
+export default function Contacts() {
   return (
     <>
       <Contact />
