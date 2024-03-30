6c06e26f4ee06fcc1c463723d6c5b8c1b1c29788
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-30 10:40:54 +0200

    Update TanstackProvider.tsx

--- src/app/components/providers/TanstackProvider.tsx
+++ src/app/components/providers/TanstackProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools/production';
 import { useMemo } from 'react';
 
 export default function TanstackProvider({
