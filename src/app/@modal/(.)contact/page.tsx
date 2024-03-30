804e222ecf5e4cf6bdd3fefbdc0e5afb890d4f5f
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-30 13:22:13 +0200

    Update page.tsx

--- src/app/@modal/(.)contact/page.tsx
+++ src/app/@modal/(.)contact/page.tsx
@@ -1,9 +1,32 @@
-import { Modal } from '@/app/components/Modal/Modal';
+'use client';
+
+import { type ElementRef, useEffect, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import { createPortal } from 'react-dom';
 
 export default function ContactModal() {
-  return (
-    <Modal>
-      <div>asd</div>
-    </Modal>
+  const router = useRouter();
+  const dialogRef = useRef<ElementRef<'dialog'>>(null);
+
+  useEffect(() => {
+    document.documentElement.className = 'no-scroll';
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+    }
+  }, []);
+
+  function onDismiss() {
+    document.documentElement.className = '';
+    router.back();
+  }
+
+  return createPortal(
+    <div className="modal-backdrop">
+      <dialog ref={dialogRef} className="modal" onClose={onDismiss}>
+        <div>asd</div>
+        <button onClick={onDismiss} className="close-button" />
+      </dialog>
+    </div>,
+    document.getElementById('modal-root')!
   );
 }
