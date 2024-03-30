d3c9dbd9a950ee2bd083bc7760f8653db4322e0e
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-03-30 11:00:25 +0200

    Update page.tsx

--- src/app/@modal/(.)contact/page.tsx
+++ src/app/@modal/(.)contact/page.tsx
@@ -1,6 +1,6 @@
 import { Contact } from '@/app/components/Contact/Contact';
 import { Modal } from './modal';
 
-export default function PageModal({ children }: { children: React.ReactNode }) {
-  return <Modal>{children}</Modal>;
+export default function PageModal() {
+  return <Modal>.</Modal>;
 }
